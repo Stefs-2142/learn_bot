06f8a3d58d3e0376b71f968d779f419e91e701f1
Stepan Bubnov <exectrogod@gmail.com>
2020-05-31 13:52:43 +0300

    added /cat command

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import settings
 import logging
+from random import randint, choice
+from glob import glob
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
@@ -20,12 +22,45 @@ def talk_to_me(update, context):
     print(text)
     update.message.reply_text(text)
 
+def play_random_number(user_number):
+    bot_number = randint(user_number-10, user_number+10)
+    if user_number > bot_number:
+        message = f'Ты загадал {user_number}, я загадал {bot_number}, ты выиграл!'
+    elif user_number == bot_number:
+        message = f'Ты загадал {user_number} и я тоже. Ничья!'
+    else:
+        message = f'Ты загадал {user_number}, я загадал {bot_number}, ты проиграл!'
+    return message
+
+def guess_number(update, context):
+    print(context.args)
+    if context.args:
+        try:
+            user_number = int(context.args[0])
+            message = play_random_number(user_number)
+        except (TypeError, ValueError):
+            message = 'Введите целое число.'
+
+    else:
+        message = 'Введите число.'
+    update.message.reply_text(message)
+
+def send_cat_picture(update, context):
+    cat_photo_list = glob('images/cat*.jp*g')
+    cat_pick_file_name = choice(cat_photo_list)
+    chat_id = update.effective_chat.id
+    context.bot.send_photo(chat_id=chat_id, photo=open(cat_pick_file_name, 'rb'))
+
+
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler('guess', guess_number))
+    dp.add_handler(CommandHandler('cat', send_cat_picture))
+
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
 
